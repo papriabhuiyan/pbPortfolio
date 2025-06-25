b0e14fe730d2a1e3ddd9ad39327ac7f93a14eece
papriabhuiyan <56096609+papriabhuiyan@users.noreply.github.com>
2025-06-25 03:01:40 -0500

    update skills section

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -7,20 +7,20 @@ export const skills = {
     { name: 'Next.js', level: 95 },
     { name: 'HTML5', level: 90 },
     { name: 'Tailwind CSS', level: 90 },
-    { name: 'Redux', level: 70 },
+    { name: 'Redux', level: 85 },
     { name: 'Storybook', level: 70 },
   ],
   backend: [
-    { name: 'Spring Boot', level: 85 },
-    { name: 'Node.js', level: 85 },
-    { name: 'Express.js', level: 80 },
-    { name: 'FastAPI', level: 75 },
-    { name: 'Flask', level: 70 },
-    { name: 'GraphQL', level: 65 },
-    { name: 'Socket.IO', level: 60 },
+    { name: 'Spring Boot', level: 95 },
+    { name: 'Node.js', level: 95 },
+    { name: 'Express.js', level: 90 },
+    { name: 'JPA', level: 95 },
+    { name: 'Flask', level: 85 },
+    { name: 'REST APIs', level: 95 },
+    { name: 'Django', level: 80 },
   ],
   other: [
     'Git', 'GitLab', 'Docker', 'Jenkins', 'CI/CD', 'AWS', 'MongoDB', 'MySQL', 'PostgreSQL',
-    'JUnit', 'Mockito', 'React Testing Library', 'Cypress', 'AEM', 'Storyblok', 'Stripe API'
+    'JUnit', 'Mockito', 'React Testing Library', 'AEM', 'Storyblok', 'Stripe API'
   ]
 };
